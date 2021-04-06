a873f1b59f915b779f53f1825513e5b11e05774c
Guilherme Fernandes <guilherme.fernandes01@outlook.com>
2021-04-06 13:48:05 -0300

    Refactoring application

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,12 +3,12 @@ import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import swaggerUi from 'swagger-ui-express';
 
-import AppError from '@errors/AppError';
+import AppError from '@shared/errors/AppError';
 
-import routes from './routes';
-import swaggerFile from './swagger.json';
+import routes from '../../../routes';
+import swaggerFile from '../../../swagger.json';
 
-import './database';
+import '../typeorm';
 import '@shared/container';
 
 const app = express();
