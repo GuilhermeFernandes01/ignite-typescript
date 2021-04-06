de91853acf5719c180656815c3fe6f4f88c168d0
Guilherme Fernandes <guilherme.fernandes01@outlook.com>
2021-04-06 19:51:53 -0300

    User admin validations

--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 
 import CreateCarController from '@modules/cars/useCases/createCar/CreateCarController';
+import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
+import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
 
 const carsRoutes = Router();
 
 const createSpecificationsController = new CreateCarController();
 
-carsRoutes.post('/', createSpecificationsController.handle);
+carsRoutes.post(
+  '/',
+  ensureAuthenticated,
+  ensureAdmin,
+  createSpecificationsController.handle
+);
 
 export default carsRoutes;
